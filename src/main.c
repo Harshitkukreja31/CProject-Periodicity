270ddaa4d3ece25d77e11d99acd6d2345712f78c
Harshitkukreja31 <harshitkukreja07@gmail.com>
2024-09-02 16:26:28 +0530

    updated main.c (for new functionality)

--- src/main.c
+++ src/main.c
@@ -2,16 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 #include "periodic_table.h"
 #include "csv_reader.h"
-
-#include <unistd.h>
+#include "plotFunctions.h"
+#include "trends.h"
 
 
 #define MAX_INPUT_LENGTH 100
 
 
-
+void printProp(Element* element){
+    printf("Element: %s\n", element->name);
+    printf("Symbol: %s\n", element->symbol);
+    printf("Atomic Number: %d\n", element->atomic_number);
+    printf("Atomic Weight: %.4f\n", element->atomic_weight);
+    printf("Period Number: %d\n", element->periodNo);
+    printf("Group Number: %d\n", element->groupNo);
+    printf("Ionization Energy: %d\n", element->ionizationEnergy);
+    printf("Electronegativity: %.2f\n", element->electronegativity);
+    printf("ElectroAffinity: %d\n", element->electronAffinity);
+    printf("Atomic Radius: %d\n", element->atomicRadius);
+    printf("Melting Point: %d\n", element->meltingPoint);
+    printf("Boiling Point: %d\n", element->boilingPoint);
+}
 
 
 int main() {
@@ -29,46 +43,45 @@ int main() {
     }
     
     while (1) {
-        printf("Search by:\n1. Symbol\n2. Full Name\n3. Atomic Number\n4. Quit\n");
+        printf("\nMenu Options:\n");
+        printf("1. Print entire Periodic Table\n");
+        printf("2. Search by Symbol\n");
+        printf("3. Search by Full Name\n");
+        printf("4. Search by Atomic Number\n");
+        printf("5. Quit\n");
         int choice;
         printf("Enter choice: ");
         scanf("%d", &choice);
         
-        if (choice == 4) {
+        if (choice == 5) {
             break;
         }
-        
-        if (choice == 1) {
+        if(choice==1){
+            print_periodic_table(pt);
+        }
+        else if (choice == 2) {
             char symbol[3];
             printf("Enter element symbol: ");
             scanf("%2s", symbol);
             to_lowercase(symbol);  // Convert to lowercase
             Element* element = pt_get_by_symbol(pt, symbol);
             if (element) {
-                printf("Element: %s\n", element->name);
-                printf("Symbol: %s\n", element->symbol);
-                printf("Atomic Number: %d\n", element->atomic_number);
-                printf("Atomic Weight: %.4f\n", element->atomic_weight);
-
+                printProp(element);
             } else {
                 printf("Element not found.\n");
             }
-        } else if (choice == 2) {
+        } else if (choice == 3) {
             char name[50];
             printf("Enter element name: ");
             scanf(" %[^\n]", name); // Read full line including spaces
             to_lowercase(name);  // Convert to lowercase
             Element* element = pt_get_by_name(pt, name);
             if (element) {
-                printf("Element: %s\n", element->name);
-                printf("Symbol: %s\n", element->symbol);
-                printf("Atomic Number: %d\n", element->atomic_number);
-                printf("Atomic Weight: %.4f\n", element->atomic_weight);
-
+                printProp(element);
             } else {
                 printf("Element not found.\n");
             }
-        } else if (choice == 3) {
+        } else if (choice == 4) {
             int atomic_number;
             printf("Enter atomic number: ");
             if (scanf("%d", &atomic_number) != 1) {
@@ -79,16 +92,13 @@ int main() {
             }
             Element* element = pt_get_by_atomic_number(pt, atomic_number);
             if (element) {
-                printf("Element: %s\n", element->name);
-                printf("Symbol: %s\n", element->symbol);
-                printf("Atomic Number: %d\n", element->atomic_number);
-                printf("Atomic Weight: %.4f\n", element->atomic_weight);
-
+                printProp(element);
             } else {
                 printf("Element not found.\n");
             }
         }
-        if (choice == 4) {
+
+
         else {
             printf("Invalid choice. Please try again.\n");
         }
